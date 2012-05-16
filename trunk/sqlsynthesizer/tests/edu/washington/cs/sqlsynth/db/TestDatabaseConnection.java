75a19c3ddaa336b66128646507568c3f96ca9a60
racezhang@gmail.com <racezhang@gmail.com@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2012-05-16 04:34:45 +0000

    update test cases

--- trunk/sqlsynthesizer/tests/edu/washington/cs/sqlsynth/db/TestDatabaseConnection.java
+++ trunk/sqlsynthesizer/tests/edu/washington/cs/sqlsynth/db/TestDatabaseConnection.java
@@ -2,6 +2,11 @@ package edu.washington.cs.sqlsynth.db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.Collection;
+import java.util.LinkedList;
+
+import edu.washington.cs.sqlsynth.entity.TableInstance;
+import edu.washington.cs.sqlsynth.util.TableInstanceReader;
 
 import junit.framework.TestCase;
 
@@ -39,4 +44,31 @@ public class TestDatabaseConnection extends TestCase {
 	    }
 	}
 	
+	public void testCreateSampleTables() {
+		TableInstance input1 = TableInstanceReader.readTableFromFile("./dat/id_name");
+		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/id_salary");
+		TableInstance output = TableInstanceReader.readTableFromFile("./dat/id_name_salary");
+		
+		Collection<TableInstance> tables = new LinkedList<TableInstance>();
+		tables.add(input1);
+		tables.add(input2);
+		tables.add(output);
+		
+		DbConnector connector = DbConnector.instance();
+		connector.initializeInputTables(tables);
+	}
+	
+	public void testCheckSampleQuery() {
+		testCreateSampleTables();
+		TableInstance output = TableInstanceReader.readTableFromFile("./dat/id_name_salary");
+		DbConnector connector = DbConnector.instance();
+		String query = "select id_name.ID_key, id_name.Name, id_salary.Salary " +
+				"from id_name, id_salary where id_name.ID_key=id_salary.ID_key " +
+				"and id_salary.Salary=200";
+		boolean same = connector.checkSQLQueryWithOutput(output, query);
+		assertTrue(same);
+		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/id_salary");
+		boolean diff = connector.checkSQLQueryWithOutput(input2, query);
+		assertTrue(!diff);
+	}
 }
