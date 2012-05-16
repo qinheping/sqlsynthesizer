ec5b842a94b240d51d7acb239502f5d6b987d7ef
racezhang@gmail.com <racezhang@gmail.com@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2012-05-16 22:02:28 +0000

    implement aggregate expr inferer

--- trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/algorithms/AggregateExprInfer.java
+++ trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/algorithms/AggregateExprInfer.java
@@ -1,24 +1,75 @@
 package edu.washington.cs.sqlsynth.algorithms;
 
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import edu.washington.cs.sqlsynth.entity.AggregateExpr;
 import edu.washington.cs.sqlsynth.entity.TableColumn;
+import edu.washington.cs.sqlsynth.entity.TableInstance;
+import edu.washington.cs.sqlsynth.util.TableUtils;
+import edu.washington.cs.sqlsynth.util.Utils;
 
 public class AggregateExprInfer {
 
-public final SQLQueryCompletor completor;
+    private final SQLQueryCompletor completor;
 	
+    private final List<TableInstance> inputTables;
+    private final TableInstance outputTable;
+    
 	public AggregateExprInfer(SQLQueryCompletor completor) {
 		this.completor = completor;
+		this.inputTables = completor.getInputTables();
+		this.outputTable = completor.getOutputTable();
 	}
 	
+	//it is 0 based
 	public Map<Integer, AggregateExpr> inferAggregationExprs() {
-		throw new RuntimeException();
+		//every column in the output table appears in the input tables
+		//suppose there is no sql like:  select a from table group by a
+		//such sql can be replaced by :  select distinct a from table;
+		if(!this.hasUnmatchedColumns()) {
+			return Collections.emptyMap();
+		}
+		Map<Integer, AggregateExpr> aggExprs = new LinkedHashMap<Integer, AggregateExpr>();
+		List<TableColumn> outputColumns = this.outputTable.getColumns();
+		for(int i = 0; i < outputColumns.size(); i++) {
+			TableColumn c = outputColumns.get(i);
+			TableColumn column = TableUtils.findFirstMatchedColumn(c.getColumnName(), this.inputTables);
+			if(column == null) {
+				aggExprs.put(i, new AggregateExpr(c));
+			}
+		}
+		return aggExprs;
 	}
 	
 	public List<TableColumn> inferGroupbyColumns() {
-		throw new RuntimeException();
+		if(!this.hasUnmatchedColumns()) {
+			return Collections.emptyList();
+		}
+		//columns that are not matched
+		List<TableColumn> outputColumns = this.outputTable.getColumns();
+		List<TableColumn> groupBys = new LinkedList<TableColumn>();
+		for(TableColumn c : outputColumns) {
+			TableColumn column = TableUtils.findFirstMatchedColumn(c.getColumnName(), this.inputTables);
+			if(column == null) {
+				groupBys.add(column);
+			}
+		}
+		Utils.checkTrue(!groupBys.isEmpty());
+		return groupBys;
+	}
+	
+	private boolean hasUnmatchedColumns() {
+		List<TableColumn> outputColumns = this.outputTable.getColumns();
+		for(TableColumn c : outputColumns) {
+			TableColumn column = TableUtils.findFirstMatchedColumn(c.getColumnName(), this.inputTables);
+			if(column == null) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/AggregateExpr.java
+++ trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/AggregateExpr.java
@@ -1,23 +1,67 @@
 package edu.washington.cs.sqlsynth.entity;
 
-public class AggregateExpr {
+import java.util.LinkedList;
+import java.util.List;
 
-	enum AggregateType {SUM, MAX, MIN, AVG};
+public class AggregateExpr {
+	public enum AggregateType {
+        COUNT { public String toString() { return "count"; }},
+		SUM { public String toString() { return "sum"; }},
+		MAX { public String toString() { return "max"; }},
+		MIN { public String toString() { return "min"; }},
+		AVG { public String toString() { return "avg"; }}
+	}
 	
 	private final TableColumn column;
-	private final AggregateType t;
+	
+	private AggregateType t = null;
+	
+	public  AggregateExpr(TableColumn column) {
+		this.column = column;
+	}
 	
 	public  AggregateExpr(TableColumn column, AggregateType t) {
-		this.column = column;
+		this(column);
 		this.t = t;
 	}
 
 	public TableColumn getColumn() {
 		return column;
 	}
+	
+	public void setAggregateType(AggregateType t) {
+		this.t = t;
+	}
 
 	public AggregateType getT() {
 		return t;
 	}
 	
+	public boolean isComplete() {
+		return t != null;
+	}
+	
+	public List<AggregateExpr> enumerateAllExprs() {
+		List<AggregateExpr> completedExprs = new LinkedList<AggregateExpr>();
+		completedExprs.add(new AggregateExpr(column, AggregateType.COUNT));
+		if(this.column.isIntegerType()) {
+		    completedExprs.add(new AggregateExpr(column, AggregateType.SUM));
+		    completedExprs.add(new AggregateExpr(column, AggregateType.AVG));
+		    completedExprs.add(new AggregateExpr(column, AggregateType.MAX));
+		    completedExprs.add(new AggregateExpr(column, AggregateType.MIN));
+		}
+		return completedExprs;
+	}
+	
+	public String toSQL() {
+		if(!this.isComplete()) {
+			throw new RuntimeException("The aggregate expr is not completed yet.");
+		}
+		return this.t.toString() + "(" + this.column.getFullName() + ")";
+	}
+	
+	@Override
+	public String toString() {
+		return (isComplete() ? this.t.toString() : "NOT-COMPLETE") + "(" + this.column.getFullName() + ")";
+	}
 }
