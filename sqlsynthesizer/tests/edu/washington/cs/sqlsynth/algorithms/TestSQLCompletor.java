60573c782010863c58e1b6a85994ac5d6f78f5f5
cherubjywh <cherubjywh@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2012-05-18 22:56:29 +0000

    Decision tree is done

--- sqlsynthesizer/tests/edu/washington/cs/sqlsynth/algorithms/TestSQLCompletor.java
+++ sqlsynthesizer/tests/edu/washington/cs/sqlsynth/algorithms/TestSQLCompletor.java
@@ -22,6 +22,35 @@ public class TestSQLCompletor extends TestCase {
 		return new TestSuite(TestSQLCompletor.class);
 	}
 
+	public void testDTree1()
+	{
+		TableInstance input1 = TableInstanceReader.readTableFromFile("./dat/id_class");
+		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/id_enroll");
+		TableInstance output = TableInstanceReader.readTableFromFile("./dat/output_5_1_3");
+		
+		Collection<TableInstance> inputs = new LinkedList<TableInstance>();
+		inputs.add(input1);
+		inputs.add(input2);
+		SQLSkeletonCreator creator = new SQLSkeletonCreator(inputs, output);
+		SQLSkeleton skeleton = creator.inferSQLSkeleton();
+		
+		SQLQueryCompletor completor = new SQLQueryCompletor(skeleton);
+		completor.addInputTable(input1);
+		completor.addInputTable(input2);
+		completor.setOutputTable(output);
+		
+		List<SQLQuery> queries = completor.inferSQLQueries();
+		for(SQLQuery q : queries) {
+			System.out.println(q.toSQLString());
+		}
+		queries = completor.validateQueriesOnDb(queries);
+		//after validating on my sql
+		System.out.println("The final output....");
+		for(SQLQuery q : queries) {
+			System.out.println(q.toSQLString());
+		}
+	}
+	
 	public void test1() {
 		TableInstance input1 = TableInstanceReader.readTableFromFile("./dat/id_name");
 		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/id_salary");
