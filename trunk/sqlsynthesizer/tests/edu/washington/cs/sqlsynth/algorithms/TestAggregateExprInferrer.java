110c83f8020b1542d599114592f0ac8c2b80ac53
racezhang@gmail.com <racezhang@gmail.com@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2012-05-20 00:29:26 +0000

    add a test for max(salary), max(age)

--- trunk/sqlsynthesizer/tests/edu/washington/cs/sqlsynth/algorithms/TestAggregateExprInferrer.java
+++ trunk/sqlsynthesizer/tests/edu/washington/cs/sqlsynth/algorithms/TestAggregateExprInferrer.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 import edu.washington.cs.sqlsynth.entity.AggregateExpr;
+import edu.washington.cs.sqlsynth.entity.SQLQuery;
 import edu.washington.cs.sqlsynth.entity.SQLSkeleton;
 import edu.washington.cs.sqlsynth.entity.TableColumn;
 import edu.washington.cs.sqlsynth.entity.TableInstance;
@@ -73,6 +74,48 @@ public class TestAggregateExprInferrer extends TestCase {
 		
 		assertTrue(aggrExprs.size() == 1);
 		assertTrue(groupbyColumns.size() == 1);
+	}
+	
+	public void test3() {
+		TableInstance input = TableInstanceReader.readTableFromFile("./dat/multigroupby/id_salary_age");
+		TableInstance output = TableInstanceReader.readTableFromFile("./dat/multigroupby/id_max_all");
+		
+		Collection<TableInstance> inputs = new LinkedList<TableInstance>();
+		inputs.add(input);
+		SQLSkeletonCreator creator = new SQLSkeletonCreator(inputs, output);
+		SQLSkeleton skeleton = creator.inferSQLSkeleton();
+		
+		SQLQueryCompletor completor = new SQLQueryCompletor(skeleton);
+		completor.addInputTable(input);
+		completor.setOutputTable(output);
+		
+		//create the inferrer
+		AggregateExprInfer aggInfer = new AggregateExprInfer(completor);
+		Map<Integer, List<AggregateExpr>> aggrExprs = aggInfer.inferAggregationExprs();
+		List<TableColumn> groupbyColumns = aggInfer.inferGroupbyColumns();
+		
+		System.out.println("aggregate expressions:");
+		System.out.println(aggrExprs.size());
+		System.out.println(aggrExprs);
+		System.out.println("group by columns:");
+		System.out.println(groupbyColumns);
+		System.out.println("all join conditions:");
+		System.out.println(skeleton.getAllJoinConditions());
+		System.out.println("projection columns:");
+		System.out.println(skeleton.getProjectColumns());
+		
+		List<SQLQuery> queries = completor.constructQueries(skeleton, aggrExprs, groupbyColumns);
+		
+		System.out.println("Num of queries: " + queries.size());
+		for(SQLQuery sql : queries) {
+			System.out.println(sql.toSQLString());
+		}
+		
+		queries = completor.validateQueriesOnDb(queries);
+		System.out.println("After validation: ");
+		for(SQLQuery sql : queries) {
+			System.out.println(sql.toSQLString());
+		}
 	}
 	
 }
