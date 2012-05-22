8692fbe906600a3bd1b4e4aa956a23d54189c984
racezhang@gmail.com <racezhang@gmail.com@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2012-05-22 03:53:41 +0000

    implement the query node and query condition

--- trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/ConditionNode.java
+++ trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/ConditionNode.java
@@ -1,25 +1,205 @@
 package edu.washington.cs.sqlsynth.entity;
 
 import edu.washington.cs.sqlsynth.entity.QueryCondition.CONJ;
+import edu.washington.cs.sqlsynth.util.Utils;
 
+/**
+ * This class represents a single or compound predicate.
+ * 
+ * A single predicate looks like:  a > b. To represent such a predicate, this
+ * ConditionNode is a leaf node. In this case, ifLeaf = true, op = GT,
+ * leftColumn = a, rightColumn = b, righConstant = null.  conj = null,
+ * left = right = null.
+ * 
+ * Note that, to represent the comparison with a constant, the constant
+ * must be put on the right hand side (the rightContant field). For example,
+ * a predicate: 5 > a should be represented as a < 5 (move the constant to the right)  
+ * 
+ * 
+ * 
+ * A compound predicate looks like:  a > b  or  c > 10. To represent such
+ * a predicate, the ConditionNode is NOT a leaf node. It will have two
+ * children (left, right) fields.  A rough representation looks like:
+ *               or
+ *             /   \
+ *            /     \
+ *          a > b   c > 10
+ * In the above case, isLeaf = false, op = null, leftColumn= rightColumn = rightConstant = null
+ * conj = OR, left = a > b (a leaf ConditionNode), right = c > 10 (a leaf COnditionNode)
+ * */
 public class ConditionNode {
 	
-	enum OP{GT, EQ, LT}; //greater than, equal, or less than
+	enum OP{
+		GT { public String toString() { return " > "; }},
+		EQ { public String toString() { return " = "; }},
+		LT { public String toString() { return " < "; }}
+		}; //greater than, equal, or less than
 
 	private final boolean isLeaf;
 	
 	//if it is a condition node
-	private CONJ conj = null;
+	private OP op = null;
 	private TableColumn leftColumn = null;
 	private TableColumn rightColumn = null;
 	private Object rightConstant = null; //constant is always on the right side
 	
 	//if it is not a condition node
-	private OP op = null;
-	private ConditionNode left = null;
-	private ConditionNode right = null;
-	
-	public ConditionNode(boolean isLeaf) {
-		this.isLeaf = isLeaf;
+	private CONJ conj = null; //"and" or "or"
+	private ConditionNode leftNode = null;
+	private ConditionNode rightNode = null;
+		
+	//create a leaf node
+	public ConditionNode(OP op, TableColumn leftColumn, TableColumn rightColumn, Object rightConstant) {
+		this.isLeaf = true;
+		Utils.checkNotNull(op);
+		Utils.checkNotNull(leftColumn);
+		if(rightColumn == null) {
+			Utils.checkNotNull(rightConstant);
+			//check type compatibility
+			if(leftColumn.isIntegerType()) {
+				Utils.checkTrue(Utils.isInteger(rightConstant+""));
+			}
+		} else {
+			Utils.checkTrue(rightConstant == null);
+			Utils.checkTrue(leftColumn.getType().equals(rightColumn.getType()));
+		}
+		//check the type
+		if(op.equals(OP.GT) || op.equals(OP.LT)) {
+			Utils.checkTrue(leftColumn.isIntegerType(), "Only integer can be compared using < and > .");
+			if(rightColumn != null) {
+				Utils.checkTrue(rightColumn.isIntegerType(), "Only integer can be compared using < and > .");
+			} else {
+				Utils.checkTrue(Utils.isInteger(rightConstant+""), "Only integer can be compared using < and > .");
+			}
+		}
+		this.op = op;
+		this.leftColumn = leftColumn;
+		this.rightColumn = rightColumn;
+		this.rightConstant = rightConstant;
 	}
+	
+	//create a null leaf node
+	public ConditionNode(CONJ conj, ConditionNode leftNode, ConditionNode rightNode) {
+		this.isLeaf = false;
+		Utils.checkNotNull(conj);
+		Utils.checkNotNull(leftNode);
+		Utils.checkNotNull(rightNode);
+		this.conj = conj;
+		this.leftNode = leftNode;
+		this.rightNode = rightNode;
+	}
+
+	public OP getOp() {
+		return op;
+	}
+
+	public void setOp(OP op) {
+		this.op = op;
+	}
+
+	public TableColumn getLeftColumn() {
+		return leftColumn;
+	}
+
+	public void setLeftColumn(TableColumn leftColumn) {
+		Utils.checkNotNull(leftColumn);
+		this.leftColumn = leftColumn;
+	}
+
+	public TableColumn getRightColumn() {
+		return rightColumn;
+	}
+
+	public void setRightColumn(TableColumn rightColumn) {
+		this.rightColumn = rightColumn;
+	}
+
+	public Object getRightConstant() {
+		return rightConstant;
+	}
+
+	public void setRightConstant(Object rightConstant) {
+		this.rightConstant = rightConstant;
+	}
+
+	public CONJ getConj() {
+		return conj;
+	}
+
+	public void setConj(CONJ conj) {
+		Utils.checkNotNull(conj);
+		this.conj = conj;
+	}
+
+	public ConditionNode getLeftNode() {
+		return leftNode;
+	}
+
+	public void setLeftNode(ConditionNode leftNode) {
+		Utils.checkNotNull(leftNode);
+		this.leftNode = leftNode;
+	}
+
+	public ConditionNode getRightNode() {
+		return rightNode;
+	}
+
+	public void setRightNode(ConditionNode rightNode) {
+		Utils.checkNotNull(rightNode);
+		this.rightNode = rightNode;
+	}
+
+	public boolean isLeaf() {
+		return isLeaf;
+	}
+	
+	//check all invariant of this class must hold
+	private void repOK() {
+		if(this.isLeaf) {
+			Utils.checkNotNull(leftColumn);
+			Utils.checkNotNull(this.op);
+			if(rightColumn == null) {
+				Utils.checkNotNull(rightConstant);
+			} else {
+				Utils.checkTrue(rightConstant == null);
+			}
+		} else {
+			Utils.checkNotNull(conj);
+			Utils.checkNotNull(leftNode);
+			Utils.checkNotNull(rightNode);
+		}
+		// check the type
+		if (op != null) {
+			if (op.equals(OP.GT) || op.equals(OP.LT)) {
+				Utils.checkTrue(leftColumn.isIntegerType(),
+						"Only integer can be compared using < and > .");
+				if (rightColumn != null) {
+					Utils.checkTrue(rightColumn.isIntegerType(),
+							"Only integer can be compared using < and > .");
+				} else {
+					Utils.checkTrue(Utils.isInteger(rightConstant + ""),
+							"Only integer can be compared using < and > .");
+				}
+			}
+		}
+	}
+	
+	public String toSQLString() {
+		this.repOK();
+		if(this.isLeaf) {
+			String leftPart = this.leftColumn.getFullName();
+			String opStr = this.op.toString();
+			String rightPart = this.rightColumn == null ? this.rightConstant.toString() : this.rightColumn.getFullName();
+			return leftPart + opStr + rightPart;
+		} else {
+			String leftPart = "(" + this.leftNode.toSQLString() + ")";
+			String opStr = this.conj.toString();
+			String rightPart = "(" + this.rightNode.toSQLString() + ")";
+			return leftPart + opStr + rightPart;
+		}
+	}
+	
+	/**
+	 * Feel free to add other methods you need to initialize, manipulate, and update this object
+	 * */
 }
--- trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/QueryCondition.java
+++ trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/QueryCondition.java
@@ -4,7 +4,10 @@ import edu.washington.cs.sqlsynth.util.Utils;
 
 public class QueryCondition {
 	
-	enum CONJ {AND, OR};
+	enum CONJ {
+		AND { public String toString() { return " and "; }},
+		OR { public String toString() { return " or "; }}
+		};
 	
 	private final ConditionNode root;
 	
@@ -14,7 +17,7 @@ public class QueryCondition {
 	}
 	
 	public String toSQLCode() {
-		throw new RuntimeException();
+		return root.toSQLString();
 	}
 	
 }
