c6d7f0b286b98b9545a06f58523c7a7dbfdc15b3
cherubjywh <cherubjywh@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2012-05-27 06:59:09 +0000

    single table from online forum

--- sqlsynthesizer/tests/edu/washington/cs/sqlsynth/algorithms/TestSQLCompletor.java
+++ sqlsynthesizer/tests/edu/washington/cs/sqlsynth/algorithms/TestSQLCompletor.java
@@ -172,6 +172,44 @@ public class TestSQLCompletor extends TestCase {
 	}
 	
 	
+	public void testDTree5()
+	{
+		TableInstance input1 = TableInstanceReader.readTableFromFile("./dat/single_table/provide_relation");
+//		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/5_1_3/id_enroll_5_1_3");
+		TableInstance output = TableInstanceReader.readTableFromFile("./dat/single_table/output");
+		
+		Collection<TableInstance> inputs = new LinkedList<TableInstance>();
+		inputs.add(input1);
+//		inputs.add(input2);
+		SQLSkeletonCreator creator = new SQLSkeletonCreator(inputs, output);
+		SQLSkeleton skeleton = creator.inferSQLSkeleton();
+		
+		System.out.println("input 1:");
+		System.out.println(input1);
+//		System.out.println("input 2:");
+//		System.out.println(input2);
+		
+		System.out.println("number of join columns: " + skeleton.getJoinPairNum());
+		
+		SQLQueryCompletor completor = new SQLQueryCompletor(skeleton);
+		completor.addInputTable(input1);
+//		completor.addInputTable(input2);
+		completor.setOutputTable(output);
+		
+		List<SQLQuery> queries = completor.inferSQLQueries();
+		System.out.println("number of inferred queries: " + queries.size());
+		for(SQLQuery q : queries) {
+			System.out.println(q.toSQLString());
+		}
+		System.out.println();
+		queries = completor.validateQueriesOnDb(queries);
+		//after validating on my sql
+		System.out.println("The final output....");
+		for(SQLQuery q : queries) {
+			System.out.println(q.toSQLString());
+		}
+	}
+	
 	public void test1() {
 		TableInstance input1 = TableInstanceReader.readTableFromFile("./dat/id_name");
 		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/id_salary");
