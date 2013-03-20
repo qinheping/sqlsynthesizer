96d2963cf77cd738d5017d0e08d3d5079266dc00
racezhang@gmail.com <racezhang@gmail.com@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2013-03-20 16:56:01 +0000

    add not exist statement

---
+++ trunk/sqlsynthesizer/tests/edu/washington/cs/sqlsynth/entity/TestNotExistStmt.java
@@ -0,0 +1,45 @@
+package edu.washington.cs.sqlsynth.entity;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+
+import edu.washington.cs.sqlsynth.algorithms.SQLQueryCompletor;
+import edu.washington.cs.sqlsynth.algorithms.SQLSkeletonCreator;
+import edu.washington.cs.sqlsynth.util.TableInstanceReader;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class TestNotExistStmt extends TestCase {
+	
+	public static Test suite() {
+		return new TestSuite(TestNotExistStmt.class);
+	}
+	
+	public void testSimpleExistStmtCase() {
+		TableInstance input1 = TableInstanceReader.readTableFromFile("./dat/id_name");
+		TableInstance input2 = TableInstanceReader.readTableFromFile("./dat/id_salary");
+		TableInstance output = TableInstanceReader.readTableFromFile("./dat/id_name_salary_full");
+		
+		Collection<TableInstance> inputs = new LinkedList<TableInstance>();
+		inputs.add(input1);
+		inputs.add(input2);
+		SQLSkeletonCreator creator = new SQLSkeletonCreator(inputs, output);
+		SQLSkeleton skeleton = creator.inferSQLSkeleton();
+		
+		SQLQueryCompletor completor = new SQLQueryCompletor(skeleton);
+		completor.addInputTable(input1);
+		completor.addInputTable(input2);
+		completor.setOutputTable(output);
+		
+		
+		List<SQLQuery> queries = completor.inferSQLQueries();
+		for(SQLQuery q : queries) {
+			NotExistStmt stmt = new NotExistStmt(q);
+			String sql = stmt.toSQLString();
+			System.out.println(sql);
+			assertTrue(sql.startsWith("NOT EXISTS ("));
+		}
+	}
+}
