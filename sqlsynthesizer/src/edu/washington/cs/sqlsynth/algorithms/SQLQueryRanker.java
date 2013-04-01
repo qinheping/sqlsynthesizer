c9daafa322eabfca5d3b7ae1d915a1a9e2fa317d
racezhang <racezhang@gmail.com>
2013-04-01 01:39:31 +0000

    ranking complexity of a sql query

--- sqlsynthesizer/src/edu/washington/cs/sqlsynth/algorithms/SQLQueryRanker.java
+++ sqlsynthesizer/src/edu/washington/cs/sqlsynth/algorithms/SQLQueryRanker.java
@@ -1,14 +1,30 @@
 package edu.washington.cs.sqlsynth.algorithms;
 
 import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import edu.washington.cs.sqlsynth.entity.SQLQuery;
+import edu.washington.cs.sqlsynth.util.Utils;
 
 public class SQLQueryRanker {
 
-	public List<SQLQuery> rankSQLQueries(Collection<SQLQuery> inputQuries) {
-		throw new RuntimeException();
+	public static List<SQLQuery> rankSQLQueries(Collection<SQLQuery> inputQueries) {
+		Map<SQLQuery, Double> scoreMap = new LinkedHashMap<SQLQuery, Double>();
+		for(SQLQuery q : inputQueries) {
+			double score = calculateSQLQueryCost(q);
+			System.out.println(score);
+			scoreMap.put(q, score);
+		}
+		scoreMap = Utils.sortByValue(scoreMap, true);
+		List<SQLQuery> rankedQueries = new LinkedList<SQLQuery>();
+		rankedQueries.addAll(scoreMap.keySet());
+		return rankedQueries;
 	}
 	
+	public static double calculateSQLQueryCost (SQLQuery query) {
+		return (double)query.toSQLString().length();
+	}
 }
