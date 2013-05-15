c4819a45f8f532521c9966ba0ca4816fc8a34fa9
racezhang@gmail.com <racezhang@gmail.com@4df188b4-21ad-4d09-5a23-5c7023b4dda7>
2013-05-15 22:57:54 +0000

    add 2 fields

--- trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/ComparisionExpr.java
+++ trunk/sqlsynthesizer/src/edu/washington/cs/sqlsynth/entity/ComparisionExpr.java
@@ -3,6 +3,10 @@ package edu.washington.cs.sqlsynth.entity;
 import java.util.LinkedList;
 import java.util.List;
 
+import checkers.javari.quals.ThisMutable;
+
+import edu.washington.cs.sqlsynth.util.Utils;
+
 /**
  * 
  * @author sunyuyin
@@ -19,6 +23,9 @@ public class ComparisionExpr {
 	
 	private ComparisionType t = null;
 	
+    private TableColumn referColumn = null;
+    private Object referColumnValue = null;
+	
 	public static boolean moreStringOp = false;
 	
 	public ComparisionExpr(TableColumn column) {
@@ -32,6 +39,13 @@ public class ComparisionExpr {
 
 	public TableColumn getColumn() {
 		return column;
+	}
+	
+	public void setReferColumn(TableColumn c, Object columnValue) {
+		Utils.checkNotNull(c);
+		Utils.checkNotNull(columnValue);
+		this.referColumn = c;
+		this.referColumnValue = columnValue;
 	}
 	
 	public void setComparisionType(ComparisionType t) {
@@ -61,6 +75,36 @@ public class ComparisionExpr {
 //		}
 //		return 
 //	}
+	public String toSQL() {
+		if(t == null || this.referColumn == null || this.referColumnValue == null) {
+			throw new Error("Not complete");
+		}
+		StringBuilder sb = new StringBuilder();
+		sb.append("(");
+		sb.append("select ");
+		if(this.equals(ComparisionType.ISMAX)) {
+			sb.append("max");
+		} else {
+			sb.append("min");
+		}
+		sb.append("(");
+		sb.append(this.column.getFullName());
+		sb.append(")");
+		sb.append(" from ");
+		sb.append(this.column.getTableName());
+		sb.append(" where ");
+		sb.append(this.referColumn.getFullName());
+		sb.append("=");
+		if(this.referColumn.isIntegerType()) {
+			sb.append(this.referColumnValue);
+		} else {
+			sb.append("'");
+			sb.append(this.referColumnValue);
+			sb.append("'");
+		}
+		sb.append(")");
+		return sb.toString();
+	}
 	
 	public String toString() {
 		return (isComplete() ? this.t.toString() : "NOT-COMPLETE") + "(" + this.column.getFullName() + ")";
